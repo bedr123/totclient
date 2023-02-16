9b4a8f38af352aef7bc8f7ffe965e13b82174a84
bedr123 <bedrgatahafitnchito@gmail.com>
2023-02-16 16:33:49 +0100

    index

--- pages/index.vue
+++ pages/index.vue
@@ -5,9 +5,9 @@
       <Meta name="description" :content="description"></Meta>
       <Meta property="og:title" :content="title"></Meta>
       <Meta property="og:description" :content="description"></Meta>
-      <Meta property="og:image" content="/api/images/tot.png"></Meta>
+      <Meta property="og:image" content="https://testoftimes.com/api/images/tot.png"></Meta>
       <Meta property="og:url" content="https://testoftimes.com"></Meta>
-      <Link rel="apple-touch-icon" url="/api/images/apple-touch-icon.png"></Link>
+      <Link rel="apple-touch-icon" url="https://testoftimes.com/api/images/apple-touch-icon.png"></Link>
     </Head>
     <ShareModal @closeShareModal="shareModal = false" v-if="shareModal" />
     <Header ref="navbar" @buyTheGame="buyTheGame = true" @howToPlay="howToPlay = true" />
